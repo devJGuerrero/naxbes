d8c1900ec63f6f5f4d415c48585943bc1cf699ae
José Guerrero <jose.guerrero.carrizo@gmail.com>
2024-03-21 23:36:48 +0000

    feat(clean address table): module finished

---
+++ resources/views/emails/clean_address_table_failed.blade.php
@@ -0,0 +1,2 @@
+<p>{{ __('messages.emails.clean_address_table_failed.body') }}</p>
+<code>{{ $messages }}</code>
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Jobs\CleanAddressTable;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})
+    ->purpose('Display an inspiring quote')
+    ->hourly();
+
+/**
+ * Clean address table.
+ * Execution time 12:00 PM.
+ */
+Schedule::job(new CleanAddressTable)
+    ->name('Clean address table')
+    ->daily()
+    ->onOneServer()
+    ->withoutOverlapping();
